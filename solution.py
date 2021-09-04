a20e1c375aab83b3ad2d3c58f232b7f0de0cce28
SA6246 <SA6246@nyu.edu>
2021-09-04 11:16:57 -0400

    update

--- solution.py
+++ solution.py
@@ -6,41 +6,41 @@ def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
 
     #Prepare a sever socket
-    serverSocket.bind(("", port))d
+    serverSocket.bind(("127.0.0.1", port)) #use local loopback
     #Fill in start
-
+    serverSocket.listen(1) #listen to incoming TCP request
     #Fill in end
 
     while True:
         #Establish the connection
         print('Ready to serve...')
-        connectionSocket, addr = #Fill in start      #Fill in end
+        connectionSocket, addr = serverSocket.accept()
+        
         try:
-            message = #Fill in start    #Fill in end
+            message = connectionSocket.recv(1024).decode()
             filename = message.split()[1]
             f = open(filename[1:])
-            outputdata = #Fill in start     #Fill in end
-
+            print("found feeding")
+            outputdata = f.read() #Fill in start     #Fill in end
             #Send one HTTP header line into socket
             #Fill in start
-
+            connectionSocket.send("HTTP/1.1 200 OK \r\n\r\n".encode())    
             #Fill in end
-
             #Send the content of the requested file to the client
             for i in range(0, len(outputdata)):
                 connectionSocket.send(outputdata[i].encode())
-
-            connectionSocket.send("\r\n".encode())
             connectionSocket.close()
         except IOError:
             #Send response message for file not found (404)
             #Fill in start
+            print("404 Page Not Found")
+            connectionSocket.send("404 Page Not Found \r\n".encode())   
 
             #Fill in end
 
             #Close client socket
             #Fill in start
-
+            connectionSocket.close()
             #Fill in end
 
     serverSocket.close()
