dc6d7e4b0fce711b849d7997679a71842e614c24
SA6246 <sa6246@nyu.edu>
2021-09-07 19:57:15 -0400

    update

--- solution.py
+++ solution.py
@@ -16,17 +16,17 @@ def smtp_client(port = 1025, mailserver = '127.0.0.1'):
     clientSocket.connect((mailserver,port)) #pass in as a tuple (str,int)
     # Fill in end
     recv = clientSocket.recv(1024).decode()
-    print(recv)
-    if recv[:3] != '220':
-       print('220 reply not received from server.')
+    #print(recv)
+    #if recv[:3] != '220':
+     #  print('220 reply not received from server.')
 
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
-    print(recv1)
-    if recv1[:3] != '250':
-        print('250 reply not received from server.')
+    #print(recv1)
+    #if recv1[:3] != '250':
+     #   print('250 reply not received from server.')
 
     # Send MAIL FROM command and print server response.
     # Fill in start
@@ -34,7 +34,7 @@ def smtp_client(port = 1025, mailserver = '127.0.0.1'):
     clientSocket.send(MailingSender.encode())
     recv2 = clientSocket.recv(1024)
     recv2 = recv2.decode()
-    print(recv2)
+    #print(recv2)
     # Fill in end
 
     # Send RCPT TO command and print server response.
@@ -43,7 +43,7 @@ def smtp_client(port = 1025, mailserver = '127.0.0.1'):
     clientSocket.send((MailRec.encode()))
     recv3 = clientSocket.recv(1024)
     recv3 = recv3.decode()
-    print(recv3)
+    #print(recv3)
     # Fill in end
 
     # Send DATA command and print server response.
@@ -52,7 +52,7 @@ def smtp_client(port = 1025, mailserver = '127.0.0.1'):
     clientSocket.send((Datuh.encode()))
     recv4 = clientSocket.recv(1024)
     recv4 = recv4.decode()
-    print(recv4)
+    #print(recv4)
     # Fill in end
 
     # Send message data.
@@ -71,7 +71,7 @@ def smtp_client(port = 1025, mailserver = '127.0.0.1'):
     clientSocket.send((Quittin.encode()))
     recv5 = clientSocket.recv(1024)
     recv5 = recv5.decode()
-    print(recv5)
+    #print(recv5)
     # Fill in end
     clientSocket.close() 
     
